508c6830d8547cfd538e784666009a9245ad0dd2
conta <conta@GeovanaMizael>
2025-08-20 16:54:52 -0300

    Testando

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,18 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+	
+		/*main é um método auto executável em java...*/
+		/*...ele sabe que ele tem que executar sozinho quando a gente der o comando*/
+		/*apertando CRTL + ESPAÇO, a ide vai auto completar */
+		/*CTRL + S = salva o arquivo, para você não perder o que escreveu.*/
+		/*aqui você pode fazer comentários*/
+		
+		/* vai imprimir texto no console*/
+		System.out.println("meu primeiro programa em Java");
+		System.out.println("Java é orientado a objetos");
+		
+	}
+}
