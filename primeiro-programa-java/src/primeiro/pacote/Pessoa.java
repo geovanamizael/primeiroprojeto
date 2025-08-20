0ab1c990a46d02f981cd40e2e75cd5b4d883c34b
conta <conta@GeovanaMizael>
2025-08-20 19:42:52 -0300

    adicionando idade na classe pessoa

--- primeiro-programa-java/src/primeiro/pacote/Pessoa.java
+++ primeiro-programa-java/src/primeiro/pacote/Pessoa.java
@@ -2,4 +2,30 @@ package primeiro.pacote;
 
 public class Pessoa {
 
+		private String nome;
+		
+		private String sobrenome;
+		
+		private Int idade;
+		
+		public Int getIdade() {
+			return idade;
+		}
+		public void setIdade(Int idade) {
+			this.idade = idade;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getSobrenome() {
+			return sobrenome;
+		}
+		public void setSobrenome(String sobrenome) {
+			this.sobrenome = sobrenome;
+		}
+	
+		
 }
